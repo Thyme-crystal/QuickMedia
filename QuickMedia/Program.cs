054db34657f3c997d9c40914f5ea8e598c1f49a5
thyme <designerthyme@gmail.com>
2026-02-27 15:43:37 +0100

    Rename '-previous' to '-back'

--- QuickMedia/Program.cs
+++ QuickMedia/Program.cs
@@ -143,7 +143,7 @@ class Program
                         "-album",
                         "-timeline",
                         "-skip",
-                        "-previous",
+                        "-back",
                         "-pause",
                         "-sessions",
                         "-seek",
